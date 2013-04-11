2c67a081aed6b94a9066a59f04bd0f2e08584294
Artem Shakhov <shahov.a.a@gmail.com>
2013-04-11 16:59:02 +0400

    Add protocol for quantity-like objects

--- src/shakhov/units/core.clj
+++ src/shakhov/units/core.clj
@@ -1,6 +1,21 @@
-(ns shakhov.units.core)
+(ns shakhov.units.core
+  (:use [clojure.algo.generic :only [root-type]])
+  (:require [clojure.algo.generic.arithmetic :as ga]
+            [clojure.algo.generic.comparison :as gc]
+            [clojure.algo.generic.math-functions :as gm]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;;
+;;   Protocol for quantity-like objects: dimensions, units and quantities
+;;
+
+(defprotocol PQuantity
+  "Properties of quantity-like objects"
+  (dimension [this]
+    "Return the dimension of quantity")
+  (unit [this]
+    "Return the unit of quantity")
+  (magnitude [this]
+    "Return the magnitude of quantity expressed in its units")
+  (magnitude-in-base-units [this]
+    "Return the magnitude of quantity expressed in base units"))
+
