906aff78f6cdfa66a1c84fc0db412e0af5eec15a
Artem Shakhov <shahov.a.a@gmail.com>
2013-04-21 14:40:16 +0400

    Add more tests

--- test/shakhov/units/core_test.clj
+++ test/shakhov/units/core_test.clj
@@ -1,14 +1,16 @@
 (ns shakhov.units.core-test
-  (:refer-clojure :exclude [time force * / + - zero? pos? neg? < > =])
+  (:refer-clojure :exclude [time force * / + - zero? pos? neg? < > =
+                            sin cos tan asin acos atan exp log])
   (:use clojure.test
-        shakhov.units.core
+        [shakhov.units.core]
         [clojure.algo.generic.arithmetic :only [* / + -]]
-        [clojure.algo.generic.comparison :only [zero? pos? neg? < > =]]))
+        [clojure.algo.generic.comparison :only [zero? pos? neg? < > =]]
+        [clojure.algo.generic.math-functions :only [sin cos tan asin acos atan exp log abs sgn atan2 pow sqrt]]))
 
 (def-dimension-system structural
   length mass time)
 
-(def-dimension
+(def-dimension structural
   area            [length 2]
   volume          [length 3]
   speed           [length 1 time -1]
@@ -18,10 +20,11 @@
   pressure        [force 1 area -1]
   stress          [force 1 area -1]
   density         [mass 1 volume -1]
-  surface-density [density 1 length 1])
+  surface-density [density 1 length 1]
+  angle           [length 1 length -1])
 
 (deftest dimension-exponents
-  (is (= {} (:exponents dimensionless)))
+  (is (dimensionless? angle))
   (is (= '{length 2}
          (:exponents area)))
   (is (= '{length 3}
@@ -62,7 +65,7 @@
     (is (thrown? Exception (* 1 length)))
     (is (thrown? Exception (* length 1)))
     (is (thrown? Exception (* length d1)))
-    (is (thrown? Exception (def-dimension foobar [d1 2 length -2])))))
+    (is (thrown? Exception (def-dimension ds1 foobar [d1 2 length -2])))))
 
 (def-unit-system si-structural
   length m
@@ -82,21 +85,22 @@
   (is (= 1 (magnitude s)))
   (is (= 1 (magnitude-in-base-units s))))
 
-(def-unit mm (* 0.001 m))
-(def-unit cm (* 0.01 m))
-(def-unit dm (* 0.1 m))
-(def-unit km (* 1000 m))
-
-(def-unit g (* 0.001 kg))
-(def-unit tonne (* 1000.0 kg))
-
-(def-unit N (/ (* kg m) s s))
-(def-unit kN (* 1e3 N))
-(def-unit MN (* 1e6 N))
+(def-unit si-structural
+  mm (* 0.001 m)
+  cm (* 0.01 m)
+  dm (* 0.1 m)
+  km (* 1000 m)
+  g (* 0.001 kg)
+  tonne (* 1000.0 kg)
+  N (/ (* kg m) s s)
+  kN (* 1e3 N)
+  MN (* 1e6 N)
+  rad angle
+  deg (* (/ Math/PI 180) rad))
 
 (let [g-accel (/ (* 9.81 m) s s)]
-  (def-unit kgf (* kg g-accel))
-  (def-unit tonf (* tonne g-accel)))
+  (def-unit si-structural kgf (* kg g-accel))
+  (def-unit si-structural tonf (* tonne g-accel)))
 
 (deftest derived-units
   (is (length? mm))
@@ -109,7 +113,14 @@
   (is (= 1000.0 (:factor tonne)))
 
   (is (force? tonf))
-  (is (= 9810.0 (magnitude-in-base-units tonf))))
+  (is (= 9810.0 (magnitude-in-base-units tonf)))
+
+  (is (angle? rad))
+  (is (= 1 (magnitude rad)))
+  (is (angle? deg))
+  (is (< (- (/ Math/PI 180) 1e-6)
+         (magnitude-in-base-units deg)
+         (+ (/ Math/PI 180) 1e-6))))
 
 (deftest quantity-arithmetic
   (is (area? (* m m)))
@@ -132,3 +143,20 @@
   (is (< mm (* 10 cm) (/ m 2)))
   (is (> (/ tonne 3) (* 100 kg) (/ kg 10)))
   (is (= (* 1000 kg) (* 1 tonne))))
+
+(deftest trigonometric
+  (is (dimensionless? rad))
+  (is (< (- 1.0 1e-6) (tan (deg 45)) (+ 1.0 1e-6)))
+  (is (< (rad (- Math/PI 1e-6))
+         (deg 180)
+         (rad (+ Math/PI 1e-6)))))
+
+(deftest powers
+  (is (= m (pow m 1)))
+  (is (area? (pow m 2)))
+  (is (< (pow (cm (- 10 1e-6)) 3)
+         (pow (m 0.1) 3)
+         (pow (cm (+ 10 1e-6)) 3)))
+  (is (volume? (pow cm 3)))
+  (is (length? (pow (pow cm 3) 1/3)))
+  (is (thrown? Exception (pow m 0.234))))
