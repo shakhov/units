5399501562096a78fb63730b89fe7519b018415b
Artem Shakhov <shahov.a.a@gmail.com>
2013-09-04 23:06:23 +0400

    Add project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject shakhov.units "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Computing with physical quantities"
+  :url "http://github.com/shakhov/units"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
